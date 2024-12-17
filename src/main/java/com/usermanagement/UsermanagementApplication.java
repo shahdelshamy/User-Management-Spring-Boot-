9b78e3c0ddee5eb9f0e2d53ee8127a619bd6f03a
shahdelshamy <134215710+shahdelshamy@users.noreply.github.com>
2024-12-18 01:46:55 +0200

    Last Update

--- src/main/java/com/usermanagement/UsermanagementApplication.java
+++ src/main/java/com/usermanagement/UsermanagementApplication.java
@@ -1,7 +1,11 @@
 package com.usermanagement;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
 
 @SpringBootApplication
 public class UsermanagementApplication {
@@ -9,5 +13,6 @@ public class UsermanagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsermanagementApplication.class, args);
 	}
+
 
 }
